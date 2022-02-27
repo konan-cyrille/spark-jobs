5f37b00bfc5a00573da624d4db210957d3b4eeb0
konan-cyrille <dkonancyrille@gmail.com>
2022-02-27 08:46:10 +0100

    modif test_set-sparkSession

--- test_convert_csv_to_parquet.py
+++ test_convert_csv_to_parquet.py
@@ -2,4 +2,5 @@ import convert_csv_files_to_parquet_file
 
 class TestConvertCsvToParquet:
     def test_set_sparkSession(self):
-        assert "<class 'pyspark.sql.session.SparkSession'>" == str(type(convert_csv_files_to_parquet_file.set_SparkSession("Ma section spark")))
+        spark = convert_csv_files_to_parquet_file.set_SparkSession("Ma section spark")
+        assert spark == spark.getActiveSession()
